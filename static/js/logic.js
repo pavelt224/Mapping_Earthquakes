b8f159205d4fd1b583b0a77fdea56abefdac731c
Pavel Tenenbaum <93852380+pavelt224@users.noreply.github.com>
2022-02-16 15:49:26 -0500

    up11

--- static/js/logic.js
+++ static/js/logic.js
@@ -34,12 +34,7 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the Toronto neighborhoods GeoJSON URL.
 let torontoHoods = "https://raw.githubusercontent.com/pavelt224/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
-// Create a style for the lines.
-let myStyle = {
-	color: "blue",
-	fillColor: "yellow",
-	weight: 1
-};
+
 
 // Grabbing our GeoJSON data.
 d3.json(torontoHoods).then(function (data) {
